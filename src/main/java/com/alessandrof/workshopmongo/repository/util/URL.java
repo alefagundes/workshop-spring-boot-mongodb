9a7079bdaf1ff07287f2ecc387e42a04afd7a51e
alefagundes <alefagundes2000.af@gmail.com>
2021-12-02 00:22:21 -0300

    Query with many criteria

--- src/main/java/com/alessandrof/workshopmongo/repository/util/URL.java
+++ src/main/java/com/alessandrof/workshopmongo/repository/util/URL.java
@@ -2,6 +2,10 @@ package com.alessandrof.workshopmongo.repository.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	public static String decodeParam(String text) {
@@ -9,6 +13,16 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	public static Date convertDate(String texxtDate, Date defaltValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(texxtDate);
+		} catch (ParseException e) {
+			return defaltValue;
 		}
 	}
 }
